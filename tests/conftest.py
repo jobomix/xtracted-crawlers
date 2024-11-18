8ead6dd9cc3b848b552454a17a4b7876221d6d6b
Bruno Abitbol <bruno.abitbol@flawlessai.com>
2024-11-18 16:59:09 +0000

    refactor with xtracted test lib

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Any, AsyncGenerator
+
+import pytest
+from pydantic import RedisDsn
+from redis.asyncio.client import Redis
+from xtracted_common.configuration import XtractedConfig
+from xtracted_common.services.jobs_service import DefaultJobsService, JobsService
+
+from xtracted.queue import Queue, RedisQueue
+
+logger = logging.getLogger(__name__)
+
+pytest_plugins = 'xtracted_tests.fixtures'
+
+
+class TestConfig(XtractedConfig):
+    redis_cluster_url: RedisDsn = 'redis://'  # type: ignore
+    db_url: str = 'postgresql://postgres:postgres@localhost:5432/postgres'
+
+
+@pytest.fixture(scope='function')
+async def queue(conf: TestConfig, redis_client: Redis) -> AsyncGenerator[Queue, Any]:
+    yield RedisQueue(conf)
+
+
+@pytest.fixture(scope='session')
+async def conf() -> AsyncGenerator[TestConfig, Any]:
+    yield TestConfig()
+
+
+@pytest.fixture(scope='function')
+async def job_service(
+    conf: TestConfig, redis_client: Redis
+) -> AsyncGenerator[JobsService, Any]:
+    yield DefaultJobsService(config=conf)
